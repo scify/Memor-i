76d398d1104a7e9d25ac8a63be2ffe9b04758d01
Pavlos Isaris <paulisaris@gmail.com>
2016-12-23 10:15:02 +0200

    Added Logger class and relevant methods MGOR-19

--- src/main/java/org/scify/memori/helper/MemoriLogger.java
+++ src/main/java/org/scify/memori/helper/MemoriLogger.java
@@ -7,10 +7,10 @@ import java.util.logging.*;
  * Class that handles logging messages within the application
  */
 public class MemoriLogger {
+
     public static final Logger LOGGER = Logger.getLogger(MemoriLogger.class.getName());
 
     public static void initLogger() {
-
         try {
             // initialise a file and a console handler
             Handler fileHandler = new FileHandler("./memori.log");
@@ -22,14 +22,11 @@ public class MemoriLogger {
             fileHandler.setLevel(Level.ALL);
             LOGGER.setLevel(Level.ALL);
             LOGGER.config("Configuration done.");
-            LOGGER.log(Level.FINE, "Fine level logged");
+
         } catch (IOException ioe) {
             LOGGER.log(Level.SEVERE, "Error occurred in FileHandler.", ioe.getMessage());
         }
     }
 
-//    public static void log(String message, Level level) {
-//        LOGGER.log(level, message);
-//    }
 
 }
