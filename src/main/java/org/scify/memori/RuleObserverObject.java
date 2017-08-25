6cc832de3e6998dfcf7dfaa6990f93b399bb5119
Pavlos Isaris <paulisaris@gmail.com>
2017-08-25 09:47:15 +0300

    Added class to implement Observer pattern in Rules class

---
+++ src/main/java/org/scify/memori/RuleObserverObject.java
@@ -0,0 +1,11 @@
+package org.scify.memori;
+
+public class RuleObserverObject {
+    public Object parameters;
+    public String code;
+
+    public RuleObserverObject(Object parameters, String code) {
+        this.parameters = parameters;
+        this.code = code;
+    }
+}
