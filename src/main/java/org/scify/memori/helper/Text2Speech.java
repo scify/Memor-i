b120349ae4720369c53a647d9eb3fdcf085d4bc6
Pavlos Isaris <paulisaris@gmail.com>
2017-08-25 15:33:55 +0300

    Improved functionality of Text to speech class

--- src/main/java/org/scify/memori/helper/Text2Speech.java
+++ src/main/java/org/scify/memori/helper/Text2Speech.java
@@ -4,17 +4,19 @@ import com.sun.speech.freetts.Voice;
 import com.sun.speech.freetts.VoiceManager;
 
 public class Text2Speech {
-    private static final String VOICENAME_kevin = "kevin16";
-    private String text; // string to speech
 
-    public Text2Speech(String text) {
-        this.text = text;
+    private static final String VOICENAME_kevin = "kevin16";
+    VoiceManager voiceManager;
+    Voice voice;
+
+    public Text2Speech() {
+        voiceManager = VoiceManager.getInstance();
+        voice = voiceManager.getVoice(VOICENAME_kevin);
+        System.err.println(voiceManager.getVoiceInfo());
     }
 
-    public void speak() {
-        Voice voice;
-        VoiceManager voiceManager = VoiceManager.getInstance();
-        voice = voiceManager.getVoice(VOICENAME_kevin);
+    public void speak(String text) {
+        voice.getAudioPlayer().cancel();
         voice.allocate();
         voice.speak(text);
     }
