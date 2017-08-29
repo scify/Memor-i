b47d989552a1a59a910ff8366828bbdfedb62a09
Pavlos Isaris <paulisaris@gmail.com>
2017-08-29 13:45:01 +0300

    Added register player screen and functionality

---
+++ src/main/java/org/scify/memori/screens/RegisterOrLoginScreenController.java
@@ -0,0 +1,48 @@
+package org.scify.memori.screens;
+
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import org.scify.memori.fx.FXAudioEngine;
+import org.scify.memori.fx.FXRenderingEngine;
+import org.scify.memori.fx.FXSceneHandler;
+
+import static javafx.scene.input.KeyCode.ESCAPE;
+import static javafx.scene.input.KeyCode.SPACE;
+
+public class RegisterOrLoginScreenController {
+
+    private Scene primaryScene;
+
+    protected FXSceneHandler sceneHandler = new FXSceneHandler();
+    protected FXAudioEngine audioEngine = new FXAudioEngine();
+
+    public void setParameters(FXSceneHandler sceneHandler, Scene userNameScreenScene) {
+        this.primaryScene = userNameScreenScene;
+        this.sceneHandler = sceneHandler;
+        FXRenderingEngine.setGamecoverIcon(userNameScreenScene, "gameCoverImgContainer");
+
+        sceneHandler.pushScene(userNameScreenScene);
+    }
+
+    @FXML
+    protected void goToRegisterScreen(KeyEvent evt) {
+        if (evt.getCode() == ESCAPE) {
+            exitScreen();
+        } else if(evt.getCode() == SPACE) {
+            new RegisterScreen(sceneHandler);
+        }
+    }
+
+    @FXML
+    protected void goToLoginScreen(KeyEvent evt) {
+        if (evt.getCode() == ESCAPE) {
+            exitScreen();
+        }
+    }
+
+    private void exitScreen() {
+        audioEngine.pauseCurrentlyPlayingAudios();
+        sceneHandler.popScene();
+    }
+}
