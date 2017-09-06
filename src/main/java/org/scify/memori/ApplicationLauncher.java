229eb09d116a08aaabe50e3e42a9a31ad88dbc8e
Pavlos Isaris <paulisaris@gmail.com>
2017-09-06 14:01:05 +0300

    Renamed class

--- src/main/java/org/scify/memori/ApplicationLauncher.java
+++ src/main/java/org/scify/memori/ApplicationLauncher.java
@@ -5,7 +5,7 @@ import org.scify.memori.screens.MainScreen;
 /**
  * This class invokes the main JavaFX application
  */
-public class Launcher {
+public class ApplicationLauncher {
 
     public static void main(String[]args) {
         MainScreen.launch(MainScreen.class);
