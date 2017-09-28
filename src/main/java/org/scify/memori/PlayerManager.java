86ff733db6fd9527041f89d9a9933ca63a0c4a62
Pavlos Isaris <paulisaris@gmail.com>
2017-09-28 11:27:32 +0300

    Added missing character

--- src/main/java/org/scify/memori/PlayerManager.java
+++ src/main/java/org/scify/memori/PlayerManager.java
@@ -97,7 +97,7 @@ public class PlayerManager  implements Callable<String> {
     }
 
     public String getPlayerAvailability(String userName) {
-        String url = "player/availability?user_name=" + userName + "&game_flavor_pack_identifier=" + gameIdentifier + "&player_initiator_id" + String.valueOf(getPlayerId());
+        String url = "player/availability?user_name=" + userName + "&game_flavor_pack_identifier=" + gameIdentifier + "&player_initiator_id=" + String.valueOf(getPlayerId());
         return this.requestManager.doGet(url);
     }
 
